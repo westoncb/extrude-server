7f6602180dc19ffd74d7ba353d28d7028dcdfddc
Weston C. Beecroft <westoncb@gmail.com>
2020-03-25 04:25:13 -0700

    Change port to 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const INDEX = '/build/index.html';
 
 const app = express()
