0530d15f144f090b8255760fc5c237be69450dad
Weston C. Beecroft <westoncb@gmail.com>
2020-03-26 15:01:55 -0700

    better client event handling scheme

--- index.js
+++ index.js
@@ -15,9 +15,33 @@ const io = socketIO(server);
 
 io.on('connection', socket => {
     console.log('Client connected', socket.id)
+
     socket.on('disconnect', () => console.log('Client disconnected'))
-    socket.on('player_input', data => {
-        console.log("player_input", data)
-        socket.emit("player_input_relay", { ...data, blah: "test" })
+
+    socket.on('event', data => {
+        console.log("client event", data)
+
+        if (data.type.startsWith("input_")) {
+
+            socket.emit("event", { ...data, time: new Date() })
+        } else {
+
+            switch (data.type) {
+                case "player_enter_request":
+                    socket.emit("event", { ...data, type: "player_enter", time: new Date() })
+                    break;
+                case "player_exit":
+                    socket.emit("event", { ...data, time: new Date() })
+                    break;
+                default:
+                    console.log("unrecognized client event: ", data)
+                    break;
+            }
+        }
+    })
+
+    socket.on("disconnect", info => {
+        console.log("disconnect event: ", info)
+        socket.emit("event", {type: "player_exit", time: new Date()})
     })
 })
